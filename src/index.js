c445a7f851230e34aaaa96a991e8f78ba80a85e7
Nazar Pidhirnyi <nazar.pidhirnyi@gmail.com>
2021-02-05 19:54:12 +0200

    feat: solve, but not perfectly, js-brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strArr = str.split('')
+
+  if (strArr.length % 2 == 1) {
+    return false
+  }
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let counter = 0
+    let arrLength = strArr.length / 2
+
+    while (counter < arrLength) {
+      if (bracketsConfig[i][0] == bracketsConfig[i][1]) {
+
+        let index1 = strArr.indexOf(bracketsConfig[i][0])
+        strArr.splice(index1, 1)
+
+        let index2 = strArr.indexOf(bracketsConfig[i][1])
+        if (index2 + 1 > index1) {
+          strArr.splice(index2, 1)
+        } else {
+          return false
+        }
+
+        if (index1 == -1 && index2 == -1) {
+          counter += 999
+        }
+
+      } else {
+        let index1 = strArr.indexOf(bracketsConfig[i][0])
+        let index2 = strArr.indexOf(bracketsConfig[i][1])
+
+        if (index2 > index1) {
+          strArr.splice(index1, 1)
+          strArr.splice(index2 - 1, 1)
+        } else if (index1 < index2) {
+          return false
+        }
+      }
+      counter++
+    }
+  }
+
+  if (strArr.length < 1) {
+    return true
+  } else {
+    return false
+  }
 }
