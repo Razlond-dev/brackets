c5f7eb03719dcde69cf9a5ff5509d60f4bb9d00b
Nazar Pidhirnyi <nazar.pidhirnyi@gmail.com>
2021-02-19 00:36:38 +0200

    refactor: change solution for js-brackets task

--- src/index.js
+++ src/index.js
@@ -1,49 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   let strArr = str.split('')
 
-  if (strArr.length % 2 == 1) {
-    return false
-  }
-
-  for (let i = 0; i < bracketsConfig.length; i++) {
-    let counter = 0
-    let arrLength = strArr.length / 2
-
-    while (counter < arrLength) {
-      if (bracketsConfig[i][0] == bracketsConfig[i][1]) {
-
-        let index1 = strArr.indexOf(bracketsConfig[i][0])
-        strArr.splice(index1, 1)
-
-        let index2 = strArr.indexOf(bracketsConfig[i][1])
-        if (index2 + 1 > index1) {
-          strArr.splice(index2, 1)
-        } else {
-          return false
-        }
-
-        if (index1 == -1 && index2 == -1) {
-          counter += 999
-        }
-
-      } else {
-        let index1 = strArr.indexOf(bracketsConfig[i][0])
-        let index2 = strArr.indexOf(bracketsConfig[i][1])
-
-        if (index2 > index1) {
-          strArr.splice(index1, 1)
-          strArr.splice(index2 - 1, 1)
-        } else if (index1 < index2) {
-          return false
-        }
+  for (let i = 0; i < strArr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (strArr[i] == bracketsConfig[j][1] && strArr[i-1] == bracketsConfig[j][0]){
+        strArr.splice(i - 1, 2)
+        i = 0
+        break
       }
-      counter++
     }
   }
-
-  if (strArr.length < 1) {
-    return true
-  } else {
-    return false
-  }
+  
+  return strArr < 1 ? true : false
 }
